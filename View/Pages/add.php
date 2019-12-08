6c1426a07d4cb647d1cba804e067b306c8ee367c
sokhorn990 <sono.sunowner@gmail.com>
2019-12-08 11:33:36 +0700

    edit add.php

---
+++ View/Pages/add.php
@@ -0,0 +1,80 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-3"></div>
+        <div class="col-6">
+            <form action="" method="post">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="text-center">Register Form</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="input-group mb-3">
+                            <label for="moto"> <b>Moto Name</b> &nbsp; &nbsp;</label>
+                            <div class="input-group-append ml-4">
+                                <span class="input-group-text" id="basic-addon2"> <i class="material-icons"> motorcycle</i></span>
+                            </div>
+                            <input type="text" class="form-control" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                        </div>
+
+                        <div class="input-group mb-3">
+                            <label for="moto"> <b>Year of product</b> &nbsp; &nbsp;</label>
+                            <div class="input-group-append">
+                                <span class="input-group-text" id="basic-addon2"> <i class="material-icons">calendar_today</i></span>
+                            </div>
+                            <input type="text" class="form-control" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                        </div>
+                        <div class="input-group mb-3">
+                            <label for="moto"> <b>Number-plate</b> &nbsp; &nbsp;</label>
+                            <div class="input-group-append ml-2">
+                                <span class="input-group-text" id="basic-addon2"> <i class="material-icons">event_note</i></span>
+                            </div>
+                            <input type="text" class="form-control" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                        </div>
+                        <div class="input-group mb-3">
+                            <label for="moto"> <b>Name of person</b> &nbsp;</label>
+                            <div class="input-group-append">
+                                <span class="input-group-text" id="basic-addon2"><i class="material-icons">person</i></span>
+                            </div>
+                            <input type="text" class="form-control" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                        </div>
+                        <div class="input-group mb-3">
+                            <label for="moto"> <b>Phone</b> &nbsp; &nbsp; &nbsp; &nbsp; </label>
+                            <div class="input-group-append ml-5">
+                                <span class="input-group-text" id="basic-addon2"> <i class="material-icons">phone</i></span>
+                            </div>
+                            <input type="text" class="form-control" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                        </div>
+                        <div class="input-group mb-3">
+                            <label for="moto"> <b>ID Card</b>   &nbsp; &nbsp; &nbsp;</label>
+                            <div class="input-group-append ml-5">
+                                <span class="input-group-text" id="basic-addon2"> <i class="material-icons">picture_in_picture</i></span>
+                            </div>
+                            <input type="text" class="form-control" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                        </div>
+                        <div class="input-group mb-3">
+                            <label for="moto"> <b>Start Date</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</label>
+                            <div class="input-group-append ml-2">
+                                <span class="input-group-text" id="basic-addon2"><i class="material-icons">calendar_today</i></span>
+                            </div>
+                            <input type="text" class="form-control" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                        </div>
+                        <div class="input-group mb-3">
+                            <label for="moto"> <b>End Date</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </label>
+                            <div class="input-group-append ml-3">
+                                <span class="input-group-text" id="basic-addon2"><i class="material-icons">calendar_today</i></span>
+                            </div>
+                            <input type="text" class="form-control" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                        </div>
+                        <button class="btn btn-danger"  type="reset">Cancel  </button>
+                        <button class="btn btn-primary float-right"  type="reset">Register  </button>
+                        
+
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="col-3"></div>
+    </div>
+</div>
