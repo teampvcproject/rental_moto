224d1af98995fe732b8332f664281782338fae03
ITlysa <lysa.thorn@gmail.com>
2019-12-14 14:49:31 +0700

    dashboard

--- View/Pages/homePage.php
+++ View/Pages/homePage.php
@@ -1,47 +1,47 @@
-
-
 <?php
-  include "View/Dashboard/dashboard.php";
+include "View/Dashboard/dashboard.php";
 ?>
-    <!-- Lysa Thorn -->
-    <div class="container mt-5">
-      
-      <div class="row mt-5">
-        <div class="col-1"></div>
-        <div class="col-10">
-        <a href="index.php?action=add_form" class="btn btn-primary">Add New Moto</a>
-          <?php
-          if (isset($data['dashborad'])) {
-            foreach ($data['dashborad'] as $result) {
+<!-- Lysa Thorn -->
+<div class="container mt-5">
 
-              ?>
-              <table id="example" class="table table-striped table-bordered" style="width:100%">
-                <thead>
-                  <tr>
-                    <th>ID</th>
-                    <th>Moto's name</th>
-                    <th>Number-plate</th>
-                    <th>Year of product</th>
-                    <th>Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td><?php echo $result['motoId'] ?></td>
-                    <td><?php echo $result['moto_name'] ?></td>
-                    <td><?php echo $result['number_plate'] ?></td>
-                    <td><?php echo $result['year_of_product'] ?></td>
-                    <td class="center">
-                        <a class="btn btn-danger btn-sm" href="index.php?action=register&id=<?php echo $result['motoId'] ?>">Rental</a>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-          <?php
-            }
+  <div class="row mt-5">
+    <div class="col-1"></div>
+    <div class="col-10">
+      <a href="index.php?action=add_form" class="btn btn-primary">Add New Moto</a>
+
+      <table id="example" class="table table-striped table-bordered" style="width:100%">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Moto's name</th>
+            <th>Number-plate</th>
+            <th>Year of product</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <?php
+        if (isset($data['dashborad'])) {
+          foreach ($data['dashborad'] as $result) {
+
+            ?>
+            <tbody>
+              <tr>
+                <td><?php echo $result['motoId'] ?></td>
+                <td><?php echo $result['moto_name'] ?></td>
+                <td><?php echo $result['number_plate'] ?></td>
+                <td><?php echo $result['year_of_product'] ?></td>
+                <td class="center">
+                  <a class="btn btn-danger btn-sm" href="index.php?action=register&id=<?php echo $result['motoId'] ?>">Rental</a>
+                </td>
+              </tr>
+            </tbody>
+        <?php
           }
-          ?>
-        </div>
-        <div class="col-1"></div>
-      </div>
+        }
+        ?>
+      </table>
+
     </div>
+    <div class="col-1"></div>
+  </div>
+</div>
