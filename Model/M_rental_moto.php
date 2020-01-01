6f1305aa08debf8045463e0330acfdb6a8f94d4c
virak0001 <virakcambodia44@gmail.com>
2020-01-01 08:51:33 +0700

    edit rental information

--- Model/M_rental_moto.php
+++ Model/M_rental_moto.php
@@ -1,7 +1,6 @@
 <?php
   function m_get_data(){
     include "connection.php";
-    
     $query = "SELECT* FROM users us INNER JOIN moto mo ON us.motoId = mo.motoId";
     $result = mysqli_query($connection,$query);
     $rows = [];
@@ -22,7 +21,7 @@
     }
  
 
-    //function edit
+    //function insert data to edit form
     function m_edit_rental_moto(){
         include "connection.php";
         $id = $_GET['id'];
@@ -35,5 +34,18 @@
             }
         }
         return $rows;
+    }
+    //edit information
+    function edit_rental_moto(){
+        include "connection.php";
+        $id = $_POST['userID'];
+        $p_name = $_POST['p_name'];
+        $phone = $_POST['phone'];
+        $id_card = $_POST['id_card'];
+        $start_date = $_POST['start'];
+        $end_date = $_POST['end'];
+        $updata = "UPDATE users SET username='$p_name', id_card='$id_card',phonenumber='$phone',start_date='$start_date',end_date='$end_date' WHERE userId = $id";
+        $result = mysqli_query($connection, $updata);
+        return $result;
     }
 ?>
